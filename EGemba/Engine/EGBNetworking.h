1faeefefbc469376c57dfd3d55eeda11bd8fe711
Edi Surata <e.surata@gmail.com>
2018-07-06 19:37:31 +0700

    update pods

---
+++ EGemba/Engine/EGBNetworking.h
@@ -0,0 +1,13 @@
+//
+//  EGBNetworking.h
+//  EGemba
+//
+//  Created by edi s on 06/07/18.
+//  Copyright © 2018 egemba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EGBNetworking : NSObject
+
+@end
---
+++ EGemba/Engine/EGBNetworking.m
@@ -0,0 +1,20 @@
+//
+//  EGBNetworking.m
+//  EGemba
+//
+//  Created by edi s on 06/07/18.
+//  Copyright © 2018 egemba. All rights reserved.
+//
+
+#import "EGBNetworking.h"
+#import <AFNet>
+
+@interface EGBNetworking ()
+
+@property (strong, nonatomic)
+
+@end
+
+@implementation EGBNetworking
+
+@end
